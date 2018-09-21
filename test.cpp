dd4eec96755051332d96db71fc0c298ef5f94769
jackiehep <jackie_hep@sina.com>
2018-09-21 21:20:27 +0800

    submit cpp secondly

--- test.cpp
+++ test.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class Test
+{
+ private:
+   string m_sIn;
+ public:
+   Test(string sIn)
+   {
+     m_sIn=sIn;
+   }
+   void showInfo()
+   {
+     cout<<m_sIn<<endl;
+   }
+};
 int main()
 {
   cout<<"hello,world"<<endl;
+  Test test("jackie");
+  test.showInfo();
   return 1;
 }
